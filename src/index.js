d641a4b1c898a4c2f7d4370b3859e819d9ce854a
ritesh <ritesh@123>
2026-04-01 19:14:45 +0530

    middlewares and there use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const { PORT } = require('./config/server_config.js');
 const apiRouter = require('./routes/api_router.js')
+const bodyparser = require('body-parser')
+const responseTime = require('response-time')
 
 const app = express();
+
+// app.use(responseTime(function (req,res,time){
+//       console.log(time); // the time will be in miliseconds
+//       res.setHeader('x-response-time', time);
+// }));
+app.use(responseTime()) //this will add a header x-response-time to the response 
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.text())
+
 
 app.use('/api', apiRouter)
 
--- src/routes/v1/ping_router.js
+++ src/routes/v1/ping_router.js
@@ -3,6 +3,18 @@ const { ping } = require('../../controllers/ping_controller.js')
 
 const pingRouter = express.Router();
 
-pingRouter.get('/', ping)
+function m1(req,res,next){
+      console.log("m1");
+      next();
+      console.log("m1endded");
+}
+
+function m2(req,res,next){
+      console.log("m2");
+      next();
+      console.log("endded m2");
+}
+
+pingRouter.get('/',[m1,m2] ,ping)
 
 module.exports = pingRouter;
